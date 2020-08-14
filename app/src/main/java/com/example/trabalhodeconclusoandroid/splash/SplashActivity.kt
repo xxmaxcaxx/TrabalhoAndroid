7d2043ae38cbedb3bb4c76aa118f43da39a50999
Fernado Henry Hruschka <fernando_henryh@hotmail.com>
2020-08-14 18:22:10 -0300

    Remote Config

--- app/src/main/java/com/example/trabalhodeconclusoandroid/splash/SplashActivity.kt
+++ app/src/main/java/com/example/trabalhodeconclusoandroid/splash/SplashActivity.kt
@@ -3,44 +3,95 @@ package com.example.trabalhodeconclusoandroid.splash
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat.startActivity
+import com.example.trabalhodeconclusoandroid.BuildConfig
 import com.example.trabalhodeconclusoandroid.R
 import com.example.trabalhodeconclusoandroid.login.LoginActivity
 import com.example.trabalhodeconclusoandroid.main.MainActivity
 import com.example.trabalhodeconclusoandroid.utils.BaseActivity
 import com.example.trabalhodeconclusoandroid.utils.CalculaFlexTracker
+import com.example.trabalhodeconclusoandroid.utils.RemoteConfig
 import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : BaseActivity() {
-    private val TEMPO_AGUARDO_SPLASHSCREEN = 3500L
+    private val TEMPO_AGUARDO_SPLASHSCREEN = 5000L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        carregar()
-        sendDataToAnalytics()
+        RemoteConfig.remoteConfigFetch()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    RemoteConfig.getFirebaseRemoteConfig().activateFetched()
+                    val minVersionApp = RemoteConfig.getFirebaseRemoteConfig()
+                        .getLong("min_version_app")
+                        .toInt()
+                    if (minVersionApp <= BuildConfig.VERSION_CODE)
+                        continueApp()
+                    else
+                        showAlertMinVersion()
+                } else
+                    continueApp()
+            }
     }
-
-    private fun sendDataToAnalytics() {
-        val bundle = Bundle()
-        CalculaFlexTracker.trackEvent(this, bundle)
+    private fun markAppAlreadyOpen(preferences: SharedPreferences) {
+        val editor = preferences.edit()
+        editor.putBoolean("open_first", false)
+        editor.apply()
     }
-
-    private fun carregar() {
-//Carrega a animacao
+    private fun showLogin() {
+        val nextScreen = Intent(this@SplashActivity, LoginActivity::class.java)
+        startActivity(nextScreen)
+        finish()
+    }
+    private fun showSplash() {
         val anim = AnimationUtils.loadAnimation(this, R.anim.animacao_splash)
         anim.reset()
         ivLogo.clearAnimation()
-//Roda a animacao
         ivLogo.startAnimation(anim)
-//Chama a próxima tela após 3,5 segundos definido na SPLASH_DISPLAY_LENGTH
         Handler().postDelayed({
-            val proximaTela = Intent(this@SplashActivity, Splash2Activity::class.java)
-            startActivity(proximaTela)
+            val nextScreen = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(nextScreen)
             finish()
         }, TEMPO_AGUARDO_SPLASHSCREEN)
+    }
+    private fun showAlertMinVersion() {
+        AlertDialog.Builder(this)
+            .setTitle("Ops")
+            .setMessage("Você esta utilizando uma versão muito antiga do aplicativo. Deseja atualizar?")
+            .setPositiveButton("Sim") { dialog, which ->
+                var intent: Intent
+                try {
+                    intent = Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$packageName"))
+                    startActivity(intent)
+                } catch (e: android.content.ActivityNotFoundException) {
+                    intent = Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("https://play.google.com/store/apps/details?id=$packageName")
+                    )
+                    startActivity(intent)
+                }
+            }
+            .setNegativeButton("Não") { dialog, which ->
+                finish()
+            }
+            .setIcon(android.R.drawable.ic_dialog_alert)
+            .show()
+    }
+    private fun continueApp() {
+        val preferences = getSharedPreferences("user_preferences", Context.MODE_PRIVATE)
+        val isFirstOpen = preferences.getBoolean("open_first", true)
+        if (isFirstOpen) {
+            showLogin()
+        } else {
+            markAppAlreadyOpen(preferences)
+            showSplash()
+        }
     }
 }
